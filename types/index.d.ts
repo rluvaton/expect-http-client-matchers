a09cd461d21309030dc178c4ce14291654eb17e9
Tamir-M <tamirmousari@gmail.com>
2024-08-14 12:22:21 +0300

    fix: fix types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,12 +1,33 @@
+import { AxiosResponse } from 'axios';
+
 interface CustomMatchers<R> extends Record<string, any> {
   toBeSuccessful(): R;
+
+  toHave2xxStatus(): R;
 }
 
+// noinspection JSUnusedGlobalSymbols
+interface SharedMatchers<R> {
+  toBeSuccessful(): R;
+
+  toHave2xxStatus(): R;
+}
+
+// biome-ignore lint/style/noNamespace: jest-extended do this
 declare namespace jest {
   // noinspection JSUnusedGlobalSymbols
-  interface Matchers<R> {
-    toBeSuccessful(): R;
-  }
+  interface Matchers<R> extends SharedMatchers<R> {}
+
+  // noinspection JSUnusedGlobalSymbols
+  interface Expect extends CustomMatchers<any> {}
+
+  // noinspection JSUnusedGlobalSymbols
+  interface InverseAsymmetricMatchers extends Expect {}
+}
+
+declare module 'expect' {
+  // noinspection JSUnusedGlobalSymbols
+  interface Matchers<R> extends SharedMatchers<R> {}
 
   // noinspection JSUnusedGlobalSymbols
   interface Expect extends CustomMatchers<any> {}
