4849de9aab3332853610910997185347c37b6f96
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2024-08-20 17:31:41 +0300

    fix(expect-types): fix AsymmetricMatchers types did not work

--- expect.d.ts
+++ expect.d.ts
@@ -2,14 +2,10 @@ import 'expect';
 
 declare module 'expect' {
   type SharedMatchers<R> = import('./types/shared').SharedMatchers<R>;
-  type CustomMatchers<R> = import('./types/shared').CustomMatchers<R>;
 
   // noinspection JSUnusedGlobalSymbols
   interface Matchers<R> extends SharedMatchers<R> {}
 
   // noinspection JSUnusedGlobalSymbols
-  interface Expect extends CustomMatchers<any> {}
-
-  // noinspection JSUnusedGlobalSymbols
-  interface InverseAsymmetricMatchers extends Expect {}
+  interface AsymmetricMatchers extends SharedMatchers<AsymmetricMatcher_2> {}
 }
