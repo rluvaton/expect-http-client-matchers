d2c8a991642b42dd0c51b7b49870fae672d10655
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2024-08-20 14:50:27 +0300

    test: run tests on all supported adapters

--- src/utils/get-debug-info.js
+++ src/utils/get-debug-info.js
@@ -1,5 +1,3 @@
-const { getMatchingAdapter } = require('../http-clients');
-
 /**
  *
  * @param {HttpClientAdapter} adapter
--- test/matchers/toHave4xxStatus.test.js
+++ test/matchers/toHave4xxStatus.test.js
@@ -1,13 +1,13 @@
 const { toHave4xxStatus } = require('../../src/index');
 const { describe, test, before } = require('node:test');
 const { buildServer } = require('../helpers/server-helper.js');
-const axios = require('axios');
 const { expect } = require('expect');
 const { getServerUrl } = require('../helpers/server-helper');
+const { testClients } = require('../helpers/supported-clients');
 
 expect.extend({ toHave4xxStatus });
 
-describe('matcher', () => {
+describe('(.not).toHave4xxStatus', () => {
   /**
    * @type {string}
    */
@@ -17,77 +17,81 @@ describe('matcher', () => {
     await buildServer();
   });
 
-  describe('.toHave4xxStatus', () => {
-    test('passes when given a 4xx status code', async () => {
-      for (let i = 400; i <= 499; i++) {
-        const response = await axios.post(
-          `${apiUrl}/status`,
-          {
-            status: i,
-          },
-          {},
-        );
+  for (const testClient of testClients) {
+    describe(`using ${testClient.name}`, () => {
+      describe('.toHave4xxStatus', () => {
+        test('passes when given a 4xx status code', async () => {
+          for (let i = 400; i <= 499; i++) {
+            const response = await testClient.post(
+              `${apiUrl}/status`,
+              {
+                status: i,
+              },
+              {},
+            );
 
-        expect(response).toHave4xxStatus();
-      }
-    });
-
-    describe('status 200 to 399 and 500 to 599', function allTests() {
-      for (let status = 200; status <= 599; status++) {
-        if (status === 400) status = 500;
-
-        test(`fails when response have status code ${status}`, async (t) => {
-          // Should have the assert snapshot assertion
-          t.plan(1);
-
-          const response = await axios.post(`${apiUrl}/status`, {
-            status,
-          });
-
-          try {
             expect(response).toHave4xxStatus();
-          } catch (e) {
-            t.assert.snapshot(e);
           }
         });
-      }
-    });
-  });
 
-  describe('.not.toHave4xxStatus', () => {
-    test('passes when given status code not in range 400 to 499', async () => {
-      for (let status = 200; status <= 599; status++) {
-        if (status === 400) status = 500;
+        describe('status 200 to 399 and 500 to 599', function allTests() {
+          for (let status = 200; status <= 599; status++) {
+            if (status === 400) status = 500;
 
-        const response = await axios.post(
-          `${apiUrl}/status`,
-          {
-            status,
-          },
-          {},
-        );
+            test(`fails when response have status code ${status}`, async (t) => {
+              // Should have the assert snapshot assertion
+              t.plan(1);
 
-        expect(response).not.toHave4xxStatus();
-      }
-    });
+              const response = await testClient.post(`${apiUrl}/status`, {
+                status,
+              });
 
-    describe('status 400 to 499', function allTests() {
-      for (let status = 400; status <= 499; status++) {
-        test(`fails when response have status code ${status}`, async (t) => {
-          // Should have the assert snapshot assertion
-          t.plan(1);
+              try {
+                expect(response).toHave4xxStatus();
+              } catch (e) {
+                t.assert.snapshot(e);
+              }
+            });
+          }
+        });
+      });
 
-          const response = await axios.post(`${apiUrl}/status`, {
-            status,
-          });
+      describe('.not.toHave4xxStatus', () => {
+        test('passes when given status code not in range 400 to 499', async () => {
+          for (let status = 200; status <= 599; status++) {
+            if (status === 400) status = 500;
 
-          try {
+            const response = await testClient.post(
+              `${apiUrl}/status`,
+              {
+                status,
+              },
+              {},
+            );
+
             expect(response).not.toHave4xxStatus();
-          } catch (e) {
-            t.assert.snapshot(e);
           }
         });
-      }
+
+        describe('status 400 to 499', function allTests() {
+          for (let status = 400; status <= 499; status++) {
+            test(`fails when response have status code ${status}`, async (t) => {
+              // Should have the assert snapshot assertion
+              t.plan(1);
+
+              const response = await testClient.post(`${apiUrl}/status`, {
+                status,
+              });
+
+              try {
+                expect(response).not.toHave4xxStatus();
+              } catch (e) {
+                t.assert.snapshot(e);
+              }
+            });
+          }
+        });
+      });
     });
-  });
+  }
 });
